a11913060c4a3e6ebfad9c31ecf8b0a1abce13eb
smile <fuck@you.gov>
2014-01-10 16:49:59 -0600

    redid phone number parsing to no longer use regexes, bugfixes as I find them

--- crawler.tcl
+++ crawler.tcl
@@ -9,18 +9,24 @@ package require TclCurl
 ;# NOTE: we're using 1 to mean true and 0 to mean false
 ;# not sure if this is how TCL does it, but it's our convention
 
+;# TODO: I've noticed myself over-using regexes; in cases where it's not appropriate we really shouldn't use them at all
+
 ;# a regular expression to detect all urls in the text
 set url_regex {https?://(?:[a-zA-Z0-9\-_()\%]+\.)+[a-zA-Z0-9\-_()\%]+(?:/[a-zA-Z0-9\-_?&=:\./;()\%]+)*}
 
 ;# TODO crawl urls that reference other pages on the same site (relative urls) also
 ;# I think if I add an @ sign in the accepted urls I can get mailto links but I'll have to parse that out too so for the moment I'm not
-set relative_tagged_url_regex {(?:(?:href=[\"\']*)|(?:src=[\"\']*))[a-zA-Z0-9\-_?&=:\./;()%\"\']+[\"\']*}
+;#set relative_tagged_url_regex {(?:(?:href=[\"\']*)|(?:src=[\"\']*))[a-zA-Z0-9\-_?&=:\./;()%\"\']+[\"\']*}
+set relative_tagged_url_regex {(?:(?:href=[\"\']*)|(?:src=[\"\']*))[a-zA-Z0-9\-_?&=:\./;%\"\']+[\"\']*}
 
 ;# a regular expression to detect all email addresses in the text
 set email_regex {[a-zA-Z0-9\-_\.()]+@(?:[a-zA-Z0-9\-_]+\.)+[a-zA-Z0-9\-_]+}
 
-;# a regex for phone numbers
-set phone_num_regex {[0-9]?\-\ ?(?:[0-9()]{3})?[\-\ ]?[0-9]{3}[\-\ ]?[0-9]{4}}
+;# a regex for phone numbers (do we need this?)
+;# set phone_num_regex {[0-9]?\-\ ?(?:[0-9()]{3})?[\-\ ]?[0-9]{3}[\-\ ]?[0-9]{4}}
+
+;# below is a phone number regex someone on stackoverflow suggested but I'm not sure what regex format it's in (perl?)
+;# ^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$
 
 ;# a list of hosts /not/ to crawl
 set host_blacklist [list {w3.org} {charter.net} {mediawiki.org} {google.com}]
@@ -65,6 +71,130 @@ proc st_search {str substr} {
 	return $found
 }
 
+;# find and remove matching quote mark in the history
+;# returns the last occurance of current_char in the nest_chars list
+proc remove_quote_match {nest_chars current_char} {
+	;# find the matching quote mark
+	set nest_to_leave [expr {[llength $nest_chars]-1}]
+	while {$nest_to_leave>=0 && [lindex $nest_chars $nest_to_leave]!=$current_char} {
+		incr nest_to_leave -1
+	}
+	
+	;# this should never be false, but just in case... (people are bad at quote matching)
+	;# if we found a matching mark
+	if {$nest_to_leave>=0} {
+		;# remove it from the nest_chars list
+		set new_nest_chars [list]
+		for {set nest_index 0} {$nest_index<[llength $nest_chars]} {incr nest_index} {
+			if {$nest_index!=$nest_to_leave} {
+				lappend new_nest_chars [lindex $nest_chars $nest_index]
+			}
+		}
+		
+		;# return the new nest_chars list, with that entry removed
+		return $new_nest_chars
+	}
+	
+	;# TODO: somehow mark this as an error? puts some error message?
+	;# we got here and didn't return, that means there was nothing to match to, so idk what's going on
+	return $nest_chars
+}
+
+;# check if a character qualifies as a quote mark
+proc is_quote_mark {test_char} {
+	if {($test_char=={'}) || ($test_char=="\"")} {
+		return 1
+	}
+	return 0
+}
+
+;# returns true if this character is a valid delimiter for a phone number, else false
+proc is_phone_delimiter {test_char} {
+	if {($test_char=={'}) || ($test_char=="\"") || ($test_char=="\n") || ($test_char=={ })} {
+		return 1
+	}
+	return 0
+}
+
+;# parse phone numbers in a much more reliable and simple way than with regular expressions
+;# returns a list of phone numbers found in the given text
+proc parse_phone_numbers {text} {
+	;# the list of phone numbers to return
+	set phone_num_list [list]
+	
+	;# our current nesting level within quotation marks
+	set nest_level 0
+	
+	;# and a history of what chars were associated with each of the nesting levels
+	set nest_chars [list]
+	
+	;# look through the text for a phone number
+	for {set n 0} {$n<[string length $text]} {incr n} {
+		;# a phone number is defined as a sequence of at least 7 digits and any amount of non-letter characters;
+		;# delimeted by spaces, quotes, or newlines
+		
+		set this_number {}
+		
+		;# check for forward delimiter
+		if {[is_phone_delimiter [string index $text $n]]} {
+			
+			set forward_delimiter [string index $text $n]
+			
+			;# don't consider the delimiter itself anymore
+			incr n
+			
+			;# if the delimiter were quotes
+			if {[is_quote_mark $forward_delimiter]} {
+				;# if we just went into more quotes (either we weren't in any or the quote we hit didn't match the last one in the history)
+				if {($nest_level<=0) || ([string index $text $n]!=[lindex $nest_chars [expr {[llength $nest_chars]-1}]])} {
+					incr nest_level
+					lappend nest_chars $forward_delimiter
+;#					puts "nest_level=$nest_level, matching $forward_delimiter, nest_chars=$nest_chars"
+				;# if we just got out of some quotes
+				} else {
+					;# we just got out of the old quotes, remember that and re-start from the start
+					incr nest_level -1
+					set nest_chars [remove_quote_match $nest_chars $forward_delimiter]
+;#					puts "nest_level=$nest_level, matching $forward_delimiter, nest_chars=$nest_chars"
+					
+					continue
+				}
+			}
+			
+			;# eat all digits until backward delimiter (which must match the forward delimiter) is reached
+			;# or we reach the end of text or an alphabet character
+			while {($n<[string length $text]) && ($forward_delimiter!=[string index $text $n]) && (![string is alpha [string index $text $n]])} {
+				
+				;# store the parsed number in this_number
+				if {[string is digit [string index $text $n]]} {
+					set this_number "$this_number[string index $text $n]"
+				}
+				
+				;# go to the next character
+				incr n
+			}
+			
+			;# exiting the loop means we hit a delimiter
+			;# if this was a quote reduce the nest level
+			if {[is_quote_mark [string index $text $n]]} {
+				;# we just got out of the old quotes
+				incr nest_level -1
+				set nest_chars [remove_quote_match $nest_chars [string index $text $n]]
+;#				puts "nest_level=$nest_level, matching $forward_delimiter, nest_chars=$nest_chars"
+			}
+		}
+		
+		;# if the number meets the length requirement, add it to the list at this time
+		if {[string length $this_number]>=7} {
+			lappend phone_num_list $this_number
+		}
+	}
+	
+	;# TODO: check list against known area codes and remove non-matching entries
+	
+	return $phone_num_list
+}
+
 ;# END GENERAL HELPER FUNCTIONS
 
 ;# add (or update, if already existing) information about a url to the given file
@@ -78,6 +208,8 @@ proc save_info {info_file url url_list email_list phone_num_list} {
 	if {[file exists $info_file]} {
 		;# first get out the existing content
 		set file_pointer [open $info_file {r}]
+		
+		;# TODO: should we ignore blank entries here? remove them or something? they're never written back to the file anyway...
 		set info_file_content [split [read $file_pointer] "\n"]
 		close $file_pointer
 	}
@@ -181,7 +313,7 @@ proc crawl_page {url max_recursion_depth} {
 		set tagged_url [st_substr $tagged_url [expr {$equal_index+1}] [expr {[string length $tagged_url]-$equal_index-[string length {=}]}]]
 		
 		;# strip off quote marks if they're there
-		set tagged_url [string trim $tagged_url {\"}]
+		set tagged_url [string trim $tagged_url "\""]
 		set tagged_url [string trim $tagged_url {'}]
 		
 		;# now prepend the current url's domain, if this url doesn't already start with "http"
@@ -209,9 +341,10 @@ proc crawl_page {url max_recursion_depth} {
 	
 	;# look through the page text for anything matching email, phone, etc.
 	set email_list [concat {*}[lsort -unique [regexp -all -inline $email_regex $page_content]]]
-	set phone_num_list [concat {*}[lsort -unique [regexp -all -inline $phone_num_regex $page_content]]]
+;#	set phone_num_list [concat {*}[lsort -unique [regexp -all -inline $phone_num_regex $page_content]]]
+	set phone_num_list [parse_phone_numbers $page_content]
 	
-	puts "crawl_page debug 1: found [llength $url_list] urls, [llength $email_list] email addresses, [llength $phone_num_list] phone numbers" 
+	puts "crawl_page debug 1: found [llength $url_list] url(s), [llength $email_list] email addresse(s), [llength $phone_num_list] phone number(s)" 
 	
 	;# store what's found as appropriate (full names, email addresses, phone numbers, etc.)
 	save_info [file join {data} {scraped_data.txt}] $url $url_list $email_list $phone_num_list
@@ -263,7 +396,7 @@ proc crawl_page {url max_recursion_depth} {
 			
 			;# I'm not 100% sure this is desired behavior but it'll do for now
 			;# break the loop here and return up as if the user started from a higher-up position
-			return 1
+;#			return 1
 		}
 		
 		;# if the recursion failed return a failure code up
@@ -310,4 +443,7 @@ proc main {argc argv argv0} {
 ;# runtime!
 main $argc $argv $argv0
 
+;# tests
+
+;#puts [parse_phone_numbers { 56823490823 this is' a test\"''\"' '687-0987' \"12345657567567\" \"123\" '825-6543' "874/8.828"}]
 
