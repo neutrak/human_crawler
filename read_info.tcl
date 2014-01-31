e09be38ba7edcecebac38b30310c9e458375a467
smile <fuck@you.gov>
2014-01-31 11:43:41 -0600

    worked on read_info command line util, seems to be working nicely,

--- read_info.tcl
+++ read_info.tcl
@@ -5,7 +5,7 @@
 ;# read information from the given file and output it prettily
 ;# we store in plain text, the url then tcl lists of everything we parse out
 ;# returns true (1) on success, false (0) on failure
-proc read_info {info_file desired_field} {
+proc read_info {info_file desired_field_index} {
 	;# initialize a blank content array
 	set info_file_content [list]
 	
@@ -30,11 +30,56 @@ proc read_info {info_file desired_field} {
 ;#	append current_info_line " {$email_list}"
 ;#	append current_info_line " {$phone_num_list}"
 	
-	;# TODO: implement the parsing of these files so I can see what's in them!
-	puts "read_info not yet implemented!"
-;#	for {set n 0} {$n<[llength $info_file_content]} {incr n} {
-;#		
-;#	}
+	
+	;# go through every line and print out the relevant data the user asked for
+	for {set line_idx 0} {$line_idx<[llength $info_file_content]} {incr line_idx} {
+		set line_data [lindex $info_file_content $line_idx]
+		
+		;#skip blank lines
+		if {$line_data=={}} {
+			continue
+		}
+		
+		;# store the line as a list for easy access
+		set line_list [list]
+		
+		;# how many {} sets we're nestled in
+		set nest_level 0
+		
+		;# an accumulator string that must persist between loop iterations
+		set acc_string {}
+		
+		;# go through the line and collect all the relevant data
+		for {set n 0} {$n<[string length $line_data]} {incr n} {
+			;# if we've reached the end of an element, put it in the larger structure
+			if {($nest_level==0) && ([string index $line_data $n]=={ })} {
+				lappend line_list $acc_string
+				set acc_string {}
+			;# if we've hit a delimiter record that
+			} elseif {[string index $line_data $n]=="\{"} {
+				incr nest_level
+			} elseif {[string index $line_data $n]=="\}"} {
+				incr nest_level -1
+			;# otherwise this is part of the data, not control
+			} else {
+				append acc_string [string index $line_data $n]
+			}
+		}
+		
+		;# the last element got no delimiter because it reached end of data, so add it now
+		if {$nest_level==0} {
+			lappend line_list $acc_string
+			set acc_string {}
+		}
+		
+		;# if this was parsed more-or-less correctly
+		if {[llength $line_list]>4} {
+			;# get out all the data the user could ask for from the newly-parsed list
+			puts [lindex $line_list $desired_field_index]
+		} else {
+			puts "Err: Could not parse $line_data"
+		}
+	}
 	
 	return 1
 }
@@ -43,8 +88,15 @@ proc read_info {info_file desired_field} {
 ;# runtime entry point
 proc main {argc argv argv0} {
 	if {$argc<2} {
-		puts "Usage: $argv0 <file to check contents of> <desired information (email, phone_num, etc.)>"
+		puts "Usage: $argv0 <file to check contents of> <desired information (email, phone_num, etc.; direct index may also be used)>"
 		exit 0
+	}
+	
+	;# map words arguments onto field indices
+	if {[lindex $argv 1]=={email}} {
+		lset argv 1 3
+	} elseif {[lindex $argv 1]=={phone_num}} {
+		lset argv 1 4
 	}
 	
 	;# if there was an error report it
